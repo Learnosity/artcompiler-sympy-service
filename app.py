55d75b7536e2d716c239ad95321c5e2c26375d38
Jeff Dyer <jeffdyer@acm.org>
2017-01-30 16:28:37 -0700

    Cleanup

--- app.py
+++ app.py
@@ -29,29 +29,29 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+# tasks = [
+#     {
+#         'id': 1,
+#         'title': u'Buy groceries',
+#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'title': u'Learn Python',
+#         'description': u'Need to find a good Python tutorial on the web',
+#         'done': False
+#     }
+# ]
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
-        else:
-            new_task[field] = task[field]
-    return new_task
+# def make_public_task(task):
+#     new_task = {}
+#     for field in task:
+#         if field == 'id':
+#             new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+#         else:
+#             new_task[field] = task[field]
+#     return new_task
 
 def eval_math(obj):
     func = obj["func"]
@@ -74,55 +74,55 @@ def eval_math(obj):
 def get_eval():
     return jsonify(str(eval_math(request.json)))
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
-@auth.login_required
-def get_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    return jsonify( { 'task': make_public_task(task[0]) } )
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+# @auth.login_required
+# def get_task(task_id):
+#     task = filter(lambda t: t['id'] == task_id, tasks)
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify( { 'task': make_public_task(task[0]) } )
 
-@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
-@auth.login_required
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify( { 'task': make_public_task(task) } ), 201
+# @app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+# @auth.login_required
+# def create_task():
+#     if not request.json or not 'title' in request.json:
+#         abort(400)
+#     task = {
+#         'id': tasks[-1]['id'] + 1,
+#         'title': request.json['title'],
+#         'description': request.json.get('description', ""),
+#         'done': False
+#     }
+#     tasks.append(task)
+#     return jsonify( { 'task': make_public_task(task) } ), 201
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
-@auth.login_required
-def update_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify( { 'task': make_public_task(task[0]) } )
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+# @auth.login_required
+# def update_task(task_id):
+#     task = filter(lambda t: t['id'] == task_id, tasks)
+#     if len(task) == 0:
+#         abort(404)
+#     if not request.json:
+#         abort(400)
+#     if 'title' in request.json and type(request.json['title']) != unicode:
+#         abort(400)
+#     if 'description' in request.json and type(request.json['description']) is not unicode:
+#         abort(400)
+#     if 'done' in request.json and type(request.json['done']) is not bool:
+#         abort(400)
+#     task[0]['title'] = request.json.get('title', task[0]['title'])
+#     task[0]['description'] = request.json.get('description', task[0]['description'])
+#     task[0]['done'] = request.json.get('done', task[0]['done'])
+#     return jsonify( { 'task': make_public_task(task[0]) } )
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
-@auth.login_required
-def delete_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify( { 'result': True } )
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
+# @auth.login_required
+# def delete_task(task_id):
+#     task = filter(lambda t: t['id'] == task_id, tasks)
+#     if len(task) == 0:
+#         abort(404)
+#     tasks.remove(task[0])
+#     return jsonify( { 'result': True } )
 
 if __name__ == '__main__':
     app.run(debug = True)
