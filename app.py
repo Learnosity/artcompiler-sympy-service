cc14f4caab5cf2dbfb9f3430bbef61f3f21429d6
Jeff Dyer <jeffdyer@acm.org>
2020-07-15 13:16:30 -0700

    Remove logging

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def eval_math(obj):
     expr = obj['expr']
 
     if func == 'eval':
-        print(str(expr))
         kwargs = {'ln_notation': 'True', 'inv_trig_style': 'power'}
         return latex(eval(expr), **kwargs)
     elif func == 'latex':
